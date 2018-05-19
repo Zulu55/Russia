9ca9f7cdd519f824f2a82ed94c95f590b3672397
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2018-05-19 08:12:34 -0500

    Splash

--- RussiaX/RussiaX.Android/MainActivity.cs
+++ RussiaX/RussiaX.Android/MainActivity.cs
@@ -1,15 +1,15 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace RussiaX.Droid
+﻿namespace RussiaX.Droid
 {
-    [Activity(Label = "RussiaX", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    using Android.App;
+    using Android.Content.PM;
+    using Android.OS;
+
+    [Activity(
+        Label = "RussiaX", 
+        Icon = "@drawable/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -24,4 +24,3 @@ namespace RussiaX.Droid
         }
     }
 }
-
--- RussiaX/RussiaX.Android/Resources/Resource.designer.cs
+++ RussiaX/RussiaX.Android/Resources/Resource.designer.cs
@@ -2195,26 +2195,26 @@ namespace RussiaX.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02011c
-			public const int avd_hide_password_1 = 2130837788;
-			
 			// aapt resource value: 0x7f02011d
-			public const int avd_hide_password_2 = 2130837789;
+			public const int avd_hide_password_1 = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int avd_hide_password_3 = 2130837790;
+			public const int avd_hide_password_2 = 2130837790;
+			
+			// aapt resource value: 0x7f02011f
+			public const int avd_hide_password_3 = 2130837791;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02011f
-			public const int avd_show_password_1 = 2130837791;
-			
 			// aapt resource value: 0x7f020120
-			public const int avd_show_password_2 = 2130837792;
+			public const int avd_show_password_1 = 2130837792;
 			
 			// aapt resource value: 0x7f020121
-			public const int avd_show_password_3 = 2130837793;
+			public const int avd_show_password_2 = 2130837793;
+			
+			// aapt resource value: 0x7f020122
+			public const int avd_show_password_3 = 2130837794;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2795,11 +2795,11 @@ namespace RussiaX.Droid
 			// aapt resource value: 0x7f020115
 			public const int notification_icon_background = 2130837781;
 			
-			// aapt resource value: 0x7f02011a
-			public const int notification_template_icon_bg = 2130837786;
-			
 			// aapt resource value: 0x7f02011b
-			public const int notification_template_icon_low_bg = 2130837787;
+			public const int notification_template_icon_bg = 2130837787;
+			
+			// aapt resource value: 0x7f02011c
+			public const int notification_template_icon_low_bg = 2130837788;
 			
 			// aapt resource value: 0x7f020116
 			public const int notification_tile_bg = 2130837782;
@@ -2811,7 +2811,10 @@ namespace RussiaX.Droid
 			public const int Russia2018 = 2130837784;
 			
 			// aapt resource value: 0x7f020119
-			public const int twitter_logo = 2130837785;
+			public const int Splash = 2130837785;
+			
+			// aapt resource value: 0x7f02011a
+			public const int twitter_logo = 2130837786;
 			
 			static Drawable()
 			{
@@ -4777,6 +4780,9 @@ namespace RussiaX.Droid
 			
 			// aapt resource value: 0x7f0b0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131427334;
+			
+			// aapt resource value: 0x7f0b018c
+			public const int Theme_Splash = 2131427724;
 			
 			// aapt resource value: 0x7f0b011f
 			public const int ThemeOverlay_AppCompat = 2131427615;
